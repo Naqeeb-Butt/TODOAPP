8bf8cbc52233ab48633e67a8ea9065616f8d5593
Naqeeb Ur Rehman Butt <139251654+Naqeeb-Butt@users.noreply.github.com>
2025-08-05 07:54:23 -0700

    app.js

--- app.js
+++ app.js
@@ -471,3 +471,10 @@ document.addEventListener("DOMContentLoaded", () => {
   
   document.getElementById("toggleDateTime").textContent = showDateTime ? "Hide Date/Time" : "Show Date/Time";
 });
+
+
+document.addEventListener('contextmenu', event => event.preventDefault());
+document.addEventListener('keydown', event => {
+    if (event.ctrlKey && (event.key === 'u' || event.key === 's' || event.key === 'i')) {
+        event.preventDefault();
+    }
